6324a7705b595bf134f6dcf3a95d9260e72a5dca
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-06-13 15:52:44 -0300

    ok

---
+++ Assets/CambioColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioColor : MonoBehaviour
+{
+    public float speed= 1.0f;
+    public Color startColor;
+    public Color endColor;
+    float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
